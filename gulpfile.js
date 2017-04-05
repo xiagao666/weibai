39f5b9b66d745e396d4c95a99aa4f6751a2196c3
huangchaowei <huangchaowei@zbj.com>
2017-04-05 17:56:29 +0800

    前端结构调整

--- gulpfile.js
+++ gulpfile.js
@@ -25,44 +25,45 @@ var gulp = require('gulp'),
     sync = require('browser-sync');
 
 // 路径配置
-var path = {
+var config = {
     // js原文件路径
-    jsSrc: "./src/**/js/*.js",
+    jsSrc: "./"+yargs.build+"/assets/src/**/js/*.js",
     // sass源文件
-    sassSrc: "./src/**/css/*.scss",
+    sassSrc: "./"+yargs.build+"/assets/src/**/css/*.scss",
     // 图片源地址
-    imgSrc: "./src/**/img/*",
+    imgSrc: "./"+yargs.build+"/assets/src/**/img/*",
     // 字体源地址
-    fontSrc: "./src/**/font/*",
+    fontSrc: "./"+yargs.build+"/assets/src/**/font/*",
     // 模板源地址
-    htmlSrc: "./src/**/*.html",
+    htmlSrc: "./"+yargs.build+"/assets/src/**/*.html",
     // 依赖库文件地址
-    libSrc: "./src/lib/**/*",
+    libSrc: "./"+yargs.build+"/assets/src/lib/**/*",
     // 目标地址
-    dist: "./dist",
+    dist: "./"+yargs.build+"/assets/dist",
+    // boss常量
     // 入口文件
-    entry: "./gulpfile.js"
-},
-// html压缩选项
-options = {
-    removeComments: true,//清除HTML注释
-    collapseWhitespace: true,//压缩HTML
-    collapseBooleanAttributes: true,//省略布尔属性的值 <input checked="true"/> ==> <input />
-    removeEmptyAttributes: true,//删除所有空格作属性值 <input id="" /> ==> <input />
-    removeScriptTypeAttributes: true,//删除<script>的type="text/javascript"
-    removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/css"
-    minifyJS: true,//压缩页面JS
-    minifyCSS: true//压缩页面CSS
+    entry: "./gulpfile.js",
+    // html压缩选项
+    options:{
+        removeComments: true,//清除HTML注释
+        collapseWhitespace: true,//压缩HTML
+        collapseBooleanAttributes: true,//省略布尔属性的值 <input checked="true"/> ==> <input />
+        removeEmptyAttributes: true,//删除所有空格作属性值 <input id="" /> ==> <input />
+        removeScriptTypeAttributes: true,//删除<script>的type="text/javascript"
+        removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/css"
+        minifyJS: true,//压缩页面JS
+        minifyCSS: true//压缩页面CSS
+    }
 };
 
 // 编译Sass后合并压缩css文件
 gulp.task('compileCSS', function() {
-    gulp.src(path.sassSrc)
-        .pipe(change(path.sassSrc))
+    gulp.src(config.sassSrc)
+        .pipe(change(config.sassSrc))
         .pipe(sass())
         .pipe(autoprefixer())
         .pipe(css())
-        .pipe(gulp.dest(path.dist));
+        .pipe(gulp.dest(config.dist));
 });
 
 // 删除css文件
@@ -72,9 +73,9 @@ gulp.task("cleanCSS", function(cb) {
 
 // 合并压缩js文件
 gulp.task('compileJS', function() {
-    gulp.src(path.jsSrc)
+    gulp.src(config.jsSrc)
         .pipe(js())
-        .pipe(gulp.dest(path.dist));
+        .pipe(gulp.dest(config.dist));
 });
 
 // 删除js文件
@@ -84,28 +85,28 @@ gulp.task("cleanJS", function(cb) {
 
 // 编译并复制html
 gulp.task("compileHtml",function(){
-    gulp.src(path.htmlSrc)
+    gulp.src(config.htmlSrc)
         .pipe(nunjucks({}))
-        .pipe(html(options))
-        .pipe(gulp.dest(path.dist));
+        .pipe(html(config.options))
+        .pipe(gulp.dest(config.dist));
 });
 
 // 复制图片
 gulp.task("copyImg",function(){
-	gulp.src(path.imgSrc)
-		.pipe(gulp.dest(path.dist));
+	gulp.src(config.imgSrc)
+		.pipe(gulp.dest(config.dist));
 });
 
 // 复制字体文件
 gulp.task("copyFont",function(){
-	gulp.src(path.fontSrc)
-		.pipe(gulp.dest(path.dist));
+	gulp.src(config.fontSrc)
+		.pipe(gulp.dest(config.dist));
 });
 
 // 复制依赖库文件
 gulp.task("copyLib",function(){
-    gulp.src(path.libSrc)
-        .pipe(gulp.dest(path.dist+"/lib"));
+    gulp.src(config.libSrc)
+        .pipe(gulp.dest(config.dist+"/lib"));
 });
 
 // 复制静态资源
@@ -131,16 +132,29 @@ gulp.task("server",function(){
 
 // 监听任务
 gulp.task("monitor",function(){
-    gulp.watch(path.jsSrc, ["compileJS"]);
-    gulp.watch(path.sassSrc, ["compileCSS"]);
-    gulp.watch(path.htmlSrc, ["compileHtml"]);
-    gulp.watch(path.imgSrc, ["copyImg"]);
-    gulp.watch(path.fontSrc, ["copyFont"]);
-    gulp.watch(path.entry, ["default"]);
+    gulp.watch(config.jsSrc, ["compileJS"]);
+    gulp.watch(config.sassSrc, ["compileCSS"]);
+    gulp.watch(config.htmlSrc, ["compileHtml"]);
+    gulp.watch(config.imgSrc, ["copyImg"]);
+    gulp.watch(config.fontSrc, ["copyFont"]);
+    gulp.watch(config.entry, ["default"]);
 });
 
 // 默认任务
-gulp.task('default',["compileCSS", "compileJS", "compileHtml", "copy"], function() {
+gulp.task('default', function() {
+    // 增加项目时以中竖线分割
+    var _project = /www|boss/;
+
+    if (!yargs.build){
+        console.warn("缺少编译参数，使用方法：gulp --build 项目名[www|boss]");
+        return;
+    }
+    if (!_project.test(yargs.build)){
+        console.warn("项目名称不存在，当前已有项目为："+_project+"");
+        return;
+    }
+    gulp.start(["compileCSS", "compileJS", "compileHtml", "copy"]);
+
     if (yargs.s){
         // gulp.start("server");
         gulp.start("monitor");
