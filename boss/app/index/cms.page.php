0750a198cfa586de6948a7e7df7888dded04fbf4
竹风 <ZhuFZY@163.com>
2017-04-14 20:05:53 +0800

    modify cms

--- boss/app/index/cms.page.php
+++ boss/app/index/cms.page.php
@@ -2,25 +2,23 @@
 
 class index_cms extends index_base
 {
-    function __construct()
+    public function __construct()
     {
-    }
-
-    function __destruct()
-    {
+//        parent::__construct();
     }
 
     /**
- * 公司新闻
- */
+     * 公司新闻
+     */
     public function pageNews($inPath)
     {
         $dbCms = new core_db_Cms();
         $condition["type"] = 1;
-        $rs = $dbCms->queryNews($condition,1,20,"");
+        $rs = $dbCms->queryNews($condition, 1, 20, "");
         $param["cmsData"] = $rs->items;
-        return $this->render("boss/news.html",$param);
+        return $this->render("boss/news.html", $param);
     }
+
     /**
      * 品牌代理
      */
@@ -28,49 +26,53 @@ class index_cms extends index_base
     {
         $dbCms = new core_db_Cms();
         $condition["type"] = 2;
-        $rs = $dbCms->queryNews($condition,1,20,"");
+        $rs = $dbCms->queryNews($condition, 1, 20, "");
         $param["cmsData"] = $rs->items;
-        return $this->render("boss/brand.html",$param);
+        return $this->render("boss/brand.html", $param);
     }
+
     /**
-    * 技术服务
-    */
+     * 技术服务
+     */
     public function pageTech($inPath)
     {
         $dbCms = new core_db_Cms();
         $condition["type"] = 3;
-        $rs = $dbCms->queryNews($condition,1,20,"");
+        $rs = $dbCms->queryNews($condition, 1, 20, "");
         $param["cmsData"] = $rs->items;
-        return $this->render("boss/tech.html",$param);
+        return $this->render("boss/tech.html", $param);
     }
+
     /**
-    * 关于唯佰
-    */
+     * 关于唯佰
+     */
     public function pageAbout($inPath)
     {
         $dbCms = new core_db_Cms();
         $condition["type"] = 4;
-        $rs = $dbCms->queryNews($condition,1,20,"");
+        $rs = $dbCms->queryNews($condition, 1, 20, "");
         $param["cmsData"] = $rs->items;
-        return $this->render("boss/about.html",$param);
+        return $this->render("boss/about.html", $param);
     }
 
     /**
      * 查询
      */
-    public function pageQueryList($inPath) {
+    public function pageQueryList($inPath)
+    {
         $dbCms = new core_db_Cms();
         $condition["type"] = $_GET["type"];
         $page = $_GET["page"];
         $size = $_GET["size"];
-        $rs = $dbCms->queryNews($condition,$page,$size,"");
+        $rs = $dbCms->queryNews($condition, $page, $size, "");
         echo json_encode($rs);
     }
 
     /**
      * 根据ID获取信息
      */
-    public function pageGetOneById($inPath) {
+    public function pageGetOneById($inPath)
+    {
         $dbCms = new core_db_Cms();
         $condition["id"] = $_GET["cmsId"];
         $rs = $dbCms->getOneCms($condition);
@@ -80,7 +82,8 @@ class index_cms extends index_base
     /**
      * 更新cms信息
      */
-    public function pageUpdate($inPath) {
+    public function pageUpdate($inPath)
+    {
         $dbCms = new core_db_Cms();
         $condition["id"] = $_GET["cmsId"];
         $item["title"] = $_GET["title"];
@@ -88,12 +91,12 @@ class index_cms extends index_base
         //$item["img_url"] = $_GET["imgUrl"];
         //$item["content"] = $_GET["content"];
         $item["hyperlink"] = $_GET["hyperlink"];
-        $item["last_update_date"] = date("y-m-d H:i:s",time());
+        $item["last_update_date"] = date("y-m-d H:i:s", time());
         $item["content"] = $_GET["content"];
         $rs = $dbCms->updateOneNews($condition, $item);
-        if($rs){
+        if ($rs) {
             $params["success"] = true;
-        }else{
+        } else {
             $params["success"] = false;
         }
         echo json_encode($params);
@@ -102,30 +105,32 @@ class index_cms extends index_base
     /**
      * 删除
      */
-    public function pageDelete($inPath){
+    public function pageDelete($inPath)
+    {
         $dbCms = new core_db_Cms();
         $condition["id"] = $_GET["cmsId"];
         $rs = $dbCms->deleteOneCms($condition);
-        if($rs){
+        if ($rs) {
             $params["success"] = true;
-        }else{
+        } else {
             $params["success"] = false;
         }
         echo json_encode($params);
     }
 
-    public function pageAdd($inPath) {
+    public function pageAdd($inPath)
+    {
         $dbCms = new core_db_Cms();
         $data["title"] = $_GET["title"];
         $data["des"] = $_GET["des"];
         $data["hyperlink"] = $_GET["hyperlink"];
         $data["type"] = $_GET["cmsType"];
-        $data["create_date"] = date("y-m-d H:i:s",time());
-        $data["last_update_date"] = date("y-m-d H:i:s",time());
+        $data["create_date"] = date("y-m-d H:i:s", time());
+        $data["last_update_date"] = date("y-m-d H:i:s", time());
         $rs = $dbCms->addNews($data);
-        if($rs){
+        if ($rs) {
             $params["success"] = true;
-        }else{
+        } else {
             $params["success"] = true;
         }
         return json_encode($params);
