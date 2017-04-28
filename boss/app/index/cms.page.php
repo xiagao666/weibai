5842fe6c673f31d5b897ce6ed3b14f230b0ee838
竹风 <ZhuFZY@163.com>
2017-04-29 05:17:07 +0800

    modify news

--- boss/app/index/cms.page.php
+++ boss/app/index/cms.page.php
@@ -4,7 +4,7 @@ class index_cms extends index_base
 {
     public function __construct()
     {
-//        parent::__construct();
+        parent::__construct();
     }
 
     /**
@@ -12,18 +12,25 @@ class index_cms extends index_base
      */
     public function pageNews($inPath)
     {
+        $searchKey = isset($_GET['searchKey']) ? core_lib_Comm::getStr($_GET['searchKey']) : '';
+        $searchValue = isset($_GET['searchVal']) ? core_lib_Comm::getStr($_GET['searchVal']) : '';
+        $page = isset($_GET['page']) ? core_lib_Comm::getStr($_GET["page"], 'int') : 1;
+        $limit = isset($_GET['limit']) ? core_lib_Comm::getStr($_GET["limit"], 'int') : 10;
+
         $dbCms = new core_db_Cms();
-        $condition = " type=1 ";
-        $searchKey = $_GET['searchKey'];
-        $searchValue = $_GET['searchVal'];
-        if(!empty($searchKey) && !empty($searchValue)) {
-            $condition = $condition." and ".$searchKey." like '%".$searchValue."%'";
+
+        $query['type'] = 1;
+        if ($searchKey && $searchValue) {
+            $query[] = " `{$searchKey}` like '%{$searchValue}%' ";
         }
-        $rs = $dbCms->queryNews($condition, 1, 20, "");
-        $param["cmsData"] = $rs->items;
-        $param['searchKey'] = $searchKey;
-        $param['searchVal'] = $searchValue;
-        return $this->render("/news/list.html", $param);
+        $newsList = $dbCms->queryNews($query, $limit, $page);
+
+        $this->pageBar($newsList['total'], $limit, $page, '/cms/news');
+
+        $this->_params['searchKey'] = $searchKey;
+        $this->_params['searchVal'] = $searchValue;
+        $this->_params['cmsData'] = $newsList['data'];
+        return $this->render("news/list.html", $this->_params);
     }
 
     /**
@@ -127,39 +134,104 @@ class index_cms extends index_base
     }
 
     /**
-     * 删除
+     * 删除内容
      */
     public function pageDelete($inPath)
     {
-        $dbCms = new core_db_Cms();
-        $condition["id"] = $_GET["cmsId"];
-        $rs = $dbCms->deleteOneCms($condition);
-        if ($rs) {
-            $params = array("status"=>"success","msg"=>"删除新闻成功！");
-        } else {
-            $params = array("status"=>"error","msg"=>"删除新闻失败！");
+        $cmsId = isset($_REQUEST['cmsId']) ? core_lib_Comm::getStr($_REQUEST['cmsId'], 'int') : 0;// 内容ID
+
+        if (!$cmsId) {
+            return $this->alert(array('status'=>'error','msg'=>"缺少内容ID"));
         }
-        return $this->alert($params);
+        $dbCms = new core_db_Cms();
+        $cmsInfo = $dbCms->getCmsById($cmsId);
+        if ($cmsInfo === false) {
+            return $this->alert(array('status'=>'error','msg'=>"删除数据不存在"));
+        }
+        $typeName = $dbCms->getTypeNameByType($cmsInfo['type']);
+
+
+        $rs = $dbCms->deleteCmsById($cmsId);
+        core_lib_Comm::p($rs);
+        if ($rs === false) {
+            return $this->alert(array("status"=>"error","msg"=>"删除{$typeName}失败！"));
+        }
+        return $this->alert(array("status"=>"success","msg"=>"删除{$typeName}成功"));
     }
 
-    public function pageAdd($inPath)
+    /**
+     * 添加CMS内容
+     * @param $inPath
+     * @return string|voi
+     */
+    public function pageAction($inPath)
     {
+        $isEdit = isset($_REQUEST['isEdit']) ? core_lib_Comm::getStr($_REQUEST['isEdit'], 'int') : 0;//是否编辑 1 编辑 0 添加
+        $cmsId = isset($_REQUEST['cmsId']) ? core_lib_Comm::getStr($_REQUEST['cmsId'], 'int') : 0;// 内容ID
+        $type = isset($_REQUEST['type']) ? core_lib_Comm::getStr($_REQUEST['type'], 'int') : 0;//类型
+        $msg = "添加";
+
         $dbCms = new core_db_Cms();
-        $data["title"] = $_REQUEST["title"];
-        $data["des"] = $_REQUEST["des"];
-        $data["hyperlink"] = $_REQUEST["hyperlink"];
-        $data["type"] = $_REQUEST["cmsType"];
-        $data["create_date"] = date("y-m-d H:i:s", time());
-        $data["last_update_date"] = date("y-m-d H:i:s", time());
-        $data['content'] = $_REQUEST['content'];
-        $rs = $dbCms->addNews($data);
-        if ($rs) {
-            $params = array("status"=>"success","msg"=>"添加新闻成功！");
-        } else {
-            $params = array("status"=>"error","msg"=>"添加新闻失败，请稍后重试！");
+        if ($isEdit) {
+            if (!$cmsId) {
+                return $this->alert(array('status'=>'error','msg'=>"缺少内容ID"));
+            }
+            $cmsInfo = $dbCms->getCmsById($cmsId);
+            if ($cmsInfo === false) {
+                return $this->alert(array('status'=>'error','msg'=>"编辑内容不存在"));
+            }
+            $this->_params['cms'] = $cmsInfo;
+            $msg = "编辑";
         }
-        return $this->alert($params);
+
+        if ($_POST) {
+            $title = isset($_POST['title']) ? core_lib_Comm::getStr($_POST['title']) : 0;//标题
+            $des = isset($_POST['des']) ? core_lib_Comm::getStr($_POST['des']) : 0;//描述
+            $imgUrl = isset($_POST['imgUrl']) ? core_lib_Comm::getStr($_POST['imgUrl']) : 0;//图片地址
+            $content = isset($_POST['content']) ? core_lib_Comm::reMoveXss($_POST['content']) : 0;//内容
+            $type = isset($_POST['type']) ? core_lib_Comm::getStr($_POST['type'], 'int') : 0;//类型
+            $relationId = isset($_POST['relationId']) ? core_lib_Comm::getStr($_POST['relationId'], 'int') : 0;//关联新闻ID
+            $hyperlink = isset($_POST['hyperlink']) ? core_lib_Comm::getStr($_POST['hyperlink']) : 0;//链接
+            $sort = isset($_POST['sort']) ? core_lib_Comm::getStr($_POST['sort'], 'int') : 0;//排序
+
+            $typeName = $dbCms->getTypeNameByType($type);
+            $data["title"] = $title;
+            $data["des"] = $des;
+            $data["img_url"] = $imgUrl;
+            $data["content"] = $content;
+            $data["type"] = $type;
+            $data["relation_id"] = $relationId;
+            $data["hyperlink"] = $hyperlink;
+            $data["sort"] = $sort;
+            $data["create_date"] = date("y-m-d H:i:s", time());
+            if ($isEdit) {
+                $data['id'] = $cmsId;
+                $rs = $dbCms->updateCmsById($data);
+            } else {
+                $rs = $dbCms->addNews($data);
+            }
+
+            if ($rs === false) {
+                return $this->alert(array("status"=>"error", "msg"=>$msg.$typeName."失败"));
+            }
+            return $this->alert(array("status"=>"success", "msg"=>$msg.$typeName."成功"));
+        }
+
+        switch ($type) {
+            case 1://新闻
+                $tpl = "news/action.html";
+                break;
+
+        }
+
+        $this->_params['type'] = $type;
+        $this->_params['cmsId'] = $cmsId;
+        $this->_params['isEdit'] = $isEdit;
+        return $this->render($tpl, $this->_params);
     }
+
+
+
     /**
      * 针对于通用的cms 比如首页滚动图
      */
--- boss/assets/src/news/js/list.js
+++ boss/assets/src/news/js/list.js
@@ -27,51 +27,6 @@ var News = {
             });
             t.$template = $("#Jconfirm");
             // 新增/编辑新闻
-        }).on("click", ".Joperate", function() {
-            var $this = $(this),
-                msg = "",
-                id = $this.data("id");
-            msg = id ? "修改新闻信息" : "添加新闻";
-            Modal.template({
-                "id": "Jtemplate",
-                "title": msg,
-                "class": "v-news",
-                "content": t._template({
-                    "id": id
-                }),
-                "callback": function() {
-                    t.handleNew({
-                        "id": id
-                    });
-                }
-            });
-            t.$template = $("#Jtemplate");
-            t.$newForm = $("#JnewForm");
-            t.$newForm.find(".alert-error button").click(function() {
-                $(this).parent().hide();
-            });
-            $("#JtemplateBtn").removeData("dismiss").removeAttr("data-dismiss");
-            t.validate();
-            t.intWangEditor();
-            if (id){
-                t.postData({
-                    "url": "/cms/getonebyid",
-                    "type": "get",
-                    "data": {"cmsId": id, "json":1},
-                    "callback": function(res){
-                        if (res.status == "success") {
-                            var data = res.data;
-                            $("#cmsId").val(data["id"]);
-                            $("#title").val(data["title"]);
-                            $("#cmsDes").val(data["des"]);
-                            $("#cmsUrl").val(data["hyperlink"]);
-                            t.wangEditor.$txt.html(data["content"]);
-                        } else {
-                            t.$newForm.find(".alert-error").show().find("span").html(res.msg);
-                        }
-                    }
-                });
-            }
         });
     },
     postData: function(config) {
@@ -125,122 +80,10 @@ var News = {
                     Modal.alert({
                         "id": "Jalert",
                         "type": "error",
-                        "content": "删除失败，请稍后重试！",
+                        "content": res.msg,
                         "callback": function() {}
                     });
                 }
-            }
-        });
-    },
-    // 新增/编辑新闻
-    handleNew: function(config) {
-        var t = this;
-        if (t.$newForm.validate().form()) {
-            var url = "",text = t.wangEditor.$txt.html();
-            if (config.id){
-                url = '/cms/update?content=' + text;
-            } else {
-                url = '/cms/add?content=' + text;
-            }
-            t.postData({
-                url: url,
-                type: "post",
-                data: t.$newForm.serialize(),
-                hObject: t.$template.find(".Jload"),
-                callback: function(res) {
-                    if (res.status == "success") {
-                        t.$template.modal("hide");
-                        Modal.alert({
-                            "id": "Jalert",
-                            "content": res.msg,
-                            "type": "success",
-                            "callback": function() {
-                                window.location.reload();
-                            }
-                        });
-                    } else {
-                        t.$newForm.find(".alert-error").show().find("span").html(res.msg);
-                    }
-                }
-            });
-        }
-    },
-    // 模板渲染
-    _template: function(config) {
-        var content = '<form id="JnewForm" action="#" class="form-horizontal">\
-                        <input type="hidden" name="cmsType" value="1" />\
-                        <input type="hidden" name="json" value="1" />\
-                        <input id="cmsId" name="cmsId" type="hidden" value="'+config.id+'"/>\
-                        <div class="alert alert-error hide">\
-                            <button class="close" type="button"></button>\
-                            <span>请完善相关信息</span>\
-                        </div>\
-                        <div class="control-group">\
-                            <label class="control-label">标题：</label>\
-                            <div class="controls">\
-                                <input id="title" name="title" type="text" class="span6 m-wrap" />\
-                            </div>\
-                        </div>\
-                        <div class="control-group">\
-                            <label class="control-label">简述：</label>\
-                            <div class="controls">\
-                                <textarea id="cmsDes" name="des" class="span6 m-wrap" rows="3"></textarea>\
-                            </div>\
-                        </div>\
-                        <div class="control-group">\
-                            <label class="control-label">链接地址：</label>\
-                            <div class="controls">\
-                                <input id="cmsUrl" name="hyperlink" type="text" class="span6 m-wrap" />\
-                            </div>\
-                        </div>\
-                        <div class="control-group">\
-                            <label class="control-label">内容：</label>\
-                            <div class="controls">\
-                                <div id="Jeditor"><p><br/></p></div>\
-                            </div>\
-                        </div>\
-                    </form>';
-
-        return content;
-    },
-    // 数据校验
-    validate: function() {
-        var t = this;
-        t.$newForm.validate({
-            errorElement: 'label', //default input error message container
-            errorClass: 'help-inline', // default input error message class
-            focusInvalid: false, // do not focus the last invalid input
-            rules: {
-                title: {
-                    required: true
-                }
-            },
-            messages: {
-                title: {
-                    required: "请输入新闻标题"
-                }
-            },
-
-            invalidHandler: function(event, validator) { //display error alert on form submit   
-                $('.alert-error', t.$newForm).show();
-            },
-
-            highlight: function(element) { // hightlight error inputs
-                $(element)
-                    .closest('.control-group').addClass('error'); // set error class to the control group
-            },
-
-            success: function(label) {
-                label.closest('.control-group').removeClass('error');
-                label.remove();
-            },
-
-            errorPlacement: function(error, element) {
-                error.addClass('help-small no-left-padding').insertAfter(element.closest('.span6.m-wrap'));
-            },
-
-            submitHandler: function(form) {
-                return false;
             }
         });
     }
