a2842e02383355ef7fa7b754726f30ebe2c80f08
竹风 <ZhuFZY@163.com>
2017-04-29 11:37:56 +0800

    modify main

--- www/app/index/main.page.php
+++ www/app/index/main.page.php
@@ -1,9 +1,10 @@
 <?php
 
-class index_main extends STpl
+class index_main extends index_base
 {
-    function __construct()
+    public function __construct()
     {
+        parent::__construct();
     }
 
     function __destruct()
@@ -15,6 +16,7 @@ class index_main extends STpl
      */
     public function pageIndex($inPath)
     {
+        $this->getNewest();//最新新闻
         /*$condition["type"] = 6;
         $dbCms = new core_db_Cms();
         $images = $dbCms->queryNews($condition, 1, 3);
@@ -22,49 +24,81 @@ class index_main extends STpl
         $news = $dbCms->queryNews($condition,1,3," create_date desc ");
         $params["indexImages"] = $images;
         $params["indexNews"] = $news;*/
-        return $this->render("index/index.html", $params);
+        return $this->render("index/index.html", $this->_params);
     }
 
     /**
      * 代理品牌
      */
-    public function pageBrand($inPath) {
-        $condition["type"] = 2;
+    public function pageBrand($inPath)
+    {
+        $query["type"] = 2;
+        $query['sort'] = 2;//sort 排序
+        $query['isDesc'] = 2;//倒序
         $dbCms = new core_db_Cms();
-        $rs = $dbCms->queryNews($condition);
-        $params["brands"] = $rs;
-        return $this->render("brand/index.html", $params);
+        $brands = $dbCms->queryNews($query, 16, 1);
+
+        $this->_params['brands'] =$brands['data'];
+        $this->_params['currNav'] = "brand";
+        return $this->render("brand/index.html", $this->_params);
     }
 
     /**
      * 技术服务
      */
-    public function pageTech($inPath) {
-        $condition["type"] = 3;
+    public function pageTech($inPath)
+    {
+        $query["type"] = 2;
+        $query['sort'] = 2;//sort 排序
+        $query['isDesc'] = 2;//倒序
         $dbCms = new core_db_Cms();
-        $rs = $dbCms->queryNews($condition);
-        $params["techs"] = $rs;
-        return $this->render("tech/index.html", $params);
+        $brands = $dbCms->queryNews($query, 16, 1);
+
+        $this->_params['brands'] =$brands['data'];
+        $this->_params['currNav'] = "brand";
+        return $this->render("tech/index.html", $this->_params);
     }
+
     /**
      * 关于唯佰
      */
-    public function pageAbout($inPath) {
+    public function pageAbout($inPath)
+    {
         $condition["type"] = 4;
         $dbCms = new core_db_Cms();
         $rs = $dbCms->queryNews($condition);
         $params["about"] = $rs;
         return $this->render("about/index.html", $params);
     }
+
     /**
      * 联系我们
      */
-    public function pageContact($inPath) {
+    public function pageContact($inPath)
+    {
         $condition["type"] = 5;
         $dbCms = new core_db_Cms();
         $rs = $dbCms->queryNews($condition);
         $params["contact"] = $rs;
         return $this->render("contact/index.html", $params);
+    }
+
+    /**
+     * 获取首页的新闻
+     */
+    public function getNewest()
+    {
+        $query['type'] = 1;
+        $query['sort'] = 2;//sort 排序
+        $query['isDesc'] = 2;//倒序
+        $newsList = $this->_dbCms->queryNews($query, 3, 1);
+        if ($newsList['data']) {
+            foreach ($newsList['data'] as $nek => $nev) {
+                $newsList['data'][$nek]['createYmd'] = date("[m/d] Y", strtotime($nev['create_date']));
+            }
+        }
+
+        $this->_params['newsList'] = $newsList['data'];
     }
 
 }
