3681612f695eb24cc4ce97bd9a9475fc9d2f15ad
huangchaowei <huangchaowei@zbj.com>
2017-04-29 21:40:54 +0800

    www

--- www/assets/src/index/js/main.js
+++ www/assets/src/index/js/main.js
@@ -2,7 +2,6 @@ var Main = {
 	init: function(){
 		var t = this;
 		t.bind();
-		t.initSwiper();
 	},
 	bind: function(){
 		$(document).on("hover", ".Jchild", function(){
@@ -11,19 +10,6 @@ var Main = {
 		}).on("mouseleave", ".Jchild", function(){
 			var $this = $(this);
 			$this.removeClass("active").find("ul").addClass("v-hide");
-		});
-	},
-	initSwiper: function(){
-		var mySwiper = new Swiper('.swiper-container', {
-		    // 可选选项，自动滑动
-		    autoplay: 5000,
-		    pagination: '.swiper-pagination',
-		    // 循环
-		    loop: true,
-		    // 速度
-		    speed: 300,
-		    // 底部分页可点击
-		    paginationClickable: true
 		});
 	}
 };
