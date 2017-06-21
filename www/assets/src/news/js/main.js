aabe443c952766e802cb551b3f259781038c1512
竹风 <ZhuFZY@163.com>
2017-06-21 22:43:36 +0800

    新闻列表修改

--- www/assets/src/news/js/main.js
+++ www/assets/src/news/js/main.js
@@ -10,7 +10,22 @@ var New = {
             }
         });
 		var t = this;
-	}
+        t.bind();
+	},
+    bind: function(){
+        var t = this;
+        $(document).on({
+            "mouseenter": function(){
+                var $this = $(this);
+                $this.addClass("active").siblings().removeClass("active");
+            },
+            "mouseleave": function(){
+                var $this = $(this);
+                $this.removeClass("active");
+                t._$nav.addClass("active");
+            }
+        }, ".Jnav");
+    }
 };
 $(function(){
     New.init();
